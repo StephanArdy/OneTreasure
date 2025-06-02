ede337e3286eb673453e2d134a41e074d1efd53e
stephan <ardistephan@gmail.com>
2025-06-02 09:22:14 +0800

    Initial Commit

---
+++ OneTreasure/OneTreasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneTreasureApp.swift
+//  OneTreasure
+//
+//  Created by stephan on 02/06/25.
+//
+
+import SwiftUI
+
+@main
+struct OneTreasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OneTreasureTests/OneTreasureTests.swift
@@ -0,0 +1,17 @@
+//
+//  OneTreasureTests.swift
+//  OneTreasureTests
+//
+//  Created by stephan on 02/06/25.
+//
+
+import Testing
+@testable import OneTreasure
+
+struct OneTreasureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
